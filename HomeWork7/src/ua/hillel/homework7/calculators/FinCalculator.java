b66d450d718c3fa5e59fb14e4150646eb1b214c2
Nonamare <nonamare@gmail.com>
2023-05-24 21:48:12 +0300

    Part 2 - added throws to FinCalculator

--- HomeWork7/src/ua/hillel/homework7/calculators/FinCalculator.java
+++ HomeWork7/src/ua/hillel/homework7/calculators/FinCalculator.java
@@ -8,7 +8,7 @@ public class FinCalculator extends SimpleCalculator {
      * @param currency - actually, USD only is supported
      * @return returns NBU rate for USD/UAH. For the period of war it's strictly 36.6
      */
-    public double getNBURate(String currency) {
+    public double getNBURate(String currency) throws IllegalArgumentException {
         if (!currency.equals("USD")) {
             throw new IllegalArgumentException("USD ONLY!");
         }
